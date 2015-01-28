1bb40df78067cb42e396babc1b760c6a047f60c7
lcor <lcormaw@gmail.com>
2015-01-28 11:30:40 -0500

    Fixed issues with days from previous and next months

--- calendar.php
+++ calendar.php
@@ -1,28 +1,21 @@
 <?php
 
-	// Set timezone
-	
-	
-	// Get the current day, weekday, month and year
-	$day = date('j'); # 1 to 31
-	$weekday = date('w'); # 0 (for Sunday) to 6 (for Saturday)
+	// Get the current month and year
 	$month = date('n'); #  1 to 12
 	$year = date('Y'); # eg. 2015
-	
-	// Check if $year is a leap year
-	$leap = date('L'); #returns 1 if leap
-	
+
 	// Verify which weekday is the first day of the month
 	$firstDay = date('w', mktime(0, 0, 0, $month, 1, $year)); # 0 (for Sunday) to 6 (for Saturday)
 	
-	// Verify how long is the month
+	// Verify how many days there are in the month
 	if ($month == 12) {
-		$lastDay = date('j', mktime(0, 0, 0, 1, 0, $year+1));
+		$howManyDays = date('j', mktime(0, 0, 0, 1, 0, $year+1));
 	} else {
-		$lastDay = date('j', mktime(0, 0, 0, $month+1, 0, $year));
+		$howManyDays = date('j', mktime(0, 0, 0, $month+1, 0, $year));
 	}
 	
-	$lastMonthDay = date('w', mktime(0, 0, 0, $month, $lastDay, $year));
+	// Verify which weekday is the last day of the month
+	$lastDay = date('w', mktime(0, 0, 0, $month, $howManyDays, $year));
 	
 ?>
 
@@ -36,7 +29,6 @@
 	</head>
 	<body>
 		<div id="calendar-wrap">
-			<!-- Make header into a dropdown menu -->
 			<header>
 				<h1><?php echo date('F') . ' ' . $year; ?></h1>
 			</header>
@@ -54,54 +46,48 @@
 				
 				<ul class="days">
 					<?php
+						// Initialize counter
+						$cnt = 1;
+						
 						// Create all <li class="day other-month">
-						if ($firstDay != 0) {
+						if ($cnt == 1 && $firstDay != 0) {
 							for ($i = 0; $i < $firstDay; $i++) {
 								echo 	"<li class=\"day other-month\">\n
 											<div class=\"date\">-</div>\n
 										</li>";
-							}	
+							}
 						}
 						
-					?>
-					
-					<?php
-						// Create <li class="day"> for first week
-						$cnt = 1;
+						// Create remaining list items for first week
 						for ($i = $firstDay; $i < 7; $i++) {
 							echo	"<li class=\"day\">\n
 										<div class=\"date\">$cnt</div>
 									</li>";
 							$cnt++;
 						}
-						
 					?>
 				</ul>
 				
 				<?php
-					
-					while ($cnt <= $lastDay) {
+					while ($cnt <= $howManyDays) {
 						echo "<ul class=\"days\">\n";
 						for ($i = 0; $i <7; $i++) {
 							echo	"<li class=\"day\">\n
 										<div class=\"date\">$cnt</div>
 									</li>";
 							$cnt++;
-							if ($cnt == $lastDay+1 && $lastMonthDay != 6) {
-								for ($i = 6 - $lastMonthDay; $i < 7; $i++) {
+							if ($cnt == $howManyDays+1 && $lastDay != 6) {
+								for ($i = 0; $i < (6 - $lastDay); $i++) {
 									echo 	"<li class=\"day other-month\">\n
-												<div class=\"date\">Empty</div>\n
+												<div class=\"date\">-</div>\n
 											</li>";
 								}
+								exit();
 							}
 						}
 						echo "</ul>";
 					}
-				
 				?>
-				
-				
-				
 				
 			</div><!-- end calendar -->
 		</div><!-- end calendar-wrap -->
